f9fd7f17a68a6202f46ebf0fc1e5e17e87698df3
jouc870 <sjak9227@gmail.com>
2023-02-04 17:09:08 +0900

    Update my_date.ts
    달력 만들 때 필요해서 getDays 함수 만들었고 함수가 정상적으로 작동하는지 확인하기 위해 isOdd, isFloat 함수 만들었음, 다른 곳에서도 사용하면 따로 파일 만들어서 분리할 예정

--- src/my_date.ts
+++ src/my_date.ts
@@ -1,10 +1,41 @@
+import assert from "assert";
+
+
 export class MyDate {
-    static year : number = new Date().getFullYear();
+    static date: Date = new Date();
+    private static day31 = [ 1, 3, 5, 7, 8, 10, 12 ];
 
     private constructor() {
     }
 
-    static isLeafYear() : boolean {
-        return (this.year % 400 == 0) || (this.year % 4 == 0 && this.year % 100 != 0);
+    static isLeafYear(): boolean {
+        const year = this.date.getFullYear();
+
+        return (year % 400 == 0) || (year % 4 == 0 && year % 100 != 0);
+    }
+
+    private static isFloatType(num: number) {
+        // test case
+        // 9.0
+        // 2.2
+        // 27
+
+        return (num.toString().indexOf('.') !== -1);
+    }
+
+    private static isOdd(num: number) {
+        assert (!MyDate.isFloatType(num));
+
+        return (num % 2 !== 0);
+    }
+
+    static getDays() {
+        const month: number = this.date.getMonth() + 1;
+
+        if (month === 2) {
+            return (this.isLeafYear()) ? 29 : 28;
+        }
+
+        return ((month < 8 && MyDate.isOdd(month)) || (8 <= month && !MyDate.isOdd(month))) ? 31 : 30;
     }
 }
